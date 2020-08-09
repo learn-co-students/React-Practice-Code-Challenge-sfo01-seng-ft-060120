21e893a03e472655568331a93544ccc3f1319bf1
darrickpang <dkypang@yahoo.com>
2020-08-08 17:31:55 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,68 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      sushis: [],
+      plates: [],
+      money: 100
+    }
+  }
+  componentDidMount(){
+    fetch(API)
+    .then(r => r.json())
+    .then(json => {
+      this.setState({
+        sushis: json
+      })
+    })
+  }
+
+  addPlates = (sushi) => {
+    let newPlates = this.state.plates
+    newPlates.push(sushi)
+    this.setState({
+      newPlates
+    })
+    
+  }
+
+  money = (sushi) => {
+    let remainder = this.state.money - sushi.price
+    if(this.state.money >= sushi.price){
+      this.setState({
+        money: remainder
+      })
+      return true
+    }
+    else{
+      return false 
+    }
+      
+  }
+
+  eat = (e, id) => {
+    let sushis = this.state.sushis.map(sushi => {
+       if(sushi.id === id){
+         if(this.money(sushi)){
+           sushi.img_url = "eaten"
+           this.addPlates(sushi)
+         }
+         
+      }
+      return sushi
+    })
+    this.setState({
+      sushis
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={this.state.sushis} eat={this.eat} />
+        <Table money={this.state.money} plates={this.state.plates}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,21 @@
 import React, { Fragment } from 'react'
 
 const Sushi = (props) => {
+  let {id, name, img_url, price} = props.sushi
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={(e) => props.eat(e, id)}>
         { 
           /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
+          img_url === 'eaten' ?
+          null  
           :
-            <img src={/* Give me an image source! */ } width="100%" />
+            <img src={img_url} width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   )
